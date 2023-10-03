d9c33bbbcb771890f68628256c44c5c9fd8cc935
Yale Araujo <yale850@gmail.com>
2023-10-03 17:08:39 -0300

    refactor: removed widget from the docs

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -5,7 +5,4 @@ export * from './ui/Form/Button'
 export * from './ui/Form/TextInput'
 export * from './ui/Form/Checkbox'
 
-export * from './ui/Display/Widget'
-export * from './ui/Display/Notification'
-
 export * from './ui/Icons'
