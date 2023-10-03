ec9198bdd5d2dbcc7be28abfe4f7a934d87c31fb
Yale Araujo <yale850@gmail.com>
2023-10-03 18:06:27 -0300

    feat: avatar component

---
+++ packages/ui/src/ui/Display/Avatar/styles.ts
@@ -0,0 +1,17 @@
+import { tv } from 'tailwind-variants'
+
+export const styles = tv(
+  {
+    base: 'rounded-full inline-block overflow-hidden shadow-black/40 shadow-md',
+    variants: {
+      size: {
+        sm: 'w-11 h-11',
+        md: 'w-14 h-14',
+        lg: 'w-16 h-16',
+      },
+    },
+  },
+  {
+    responsiveVariants: ['2xl', 'lg', 'md', 'sm', 'xl'],
+  },
+)
